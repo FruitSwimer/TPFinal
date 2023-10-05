cf0932996599abb7d5c827aac23edf69c63702a6
Hugo Dickodet <hugo.dickodet@gmail.com>
2023-10-05 10:36:53 +0200

    quizz with one question per page

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
+let currentQuestion = 0;
+const questions = document.querySelectorAll('.question-container');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const submitBtn = document.getElementById('submitBtn');
+
+function showQuestion(questionNumber) {
+    questions.forEach(question => {
+        question.classList.add('question-hidden');
+    });
+
+    questions[questionNumber].classList.remove('question-hidden');
+
+    if (questionNumber === 0) {
+        prevBtn.style.display = 'none';
+    } else {
+        prevBtn.style.display = 'inline-block';
+    }
+
+    if (questionNumber === questions.length - 1) {
+        nextBtn.style.display = 'none';
+        submitBtn.style.display = 'inline-block';
+    } else {
+        nextBtn.style.display = 'inline-block';
+        submitBtn.style.display = 'none';
+    }
+}
+
+function nextQuestion() {
+    if (currentQuestion < questions.length - 1) {
+        currentQuestion++;
+        showQuestion(currentQuestion);
+    }
+}
+
+function prevQuestion() {
+    if (currentQuestion > 0) {
+        currentQuestion--;
+        showQuestion(currentQuestion);
+    }
+}
+
+function checkAnswers() {
+    console.log(questions.value);
+}
+
+showQuestion(currentQuestion);
+
+
+
+
 var modal = document.getElementById('myModal');
 var btn = document.querySelector('.start-button');
 var description = document.getElementById('description')
 // var startQuizBtn = document.getElementById('startQuizBtn');
 var span = document.querySelector('.close');
-
-btn.onclick = function() {
-    modal.style.display = 'block';
-    description.style.display = "none";
-}
-
 
 window.onclick = function(event) {
     if (event.target == modal) {
@@ -28,7 +73,7 @@ form.addEventListener("submit", function (event) {
         console.log(response.value);
         // S'il est invalide, on affiche un message d'erreur personnalisé
         error.innerHTML =
-          "Mauvaise répose, retourne lire ton cours";
+          "Mauvaise réponse, retourne lire ton cours";
         error.className = "error active";
     }
     else {
@@ -36,4 +81,6 @@ form.addEventListener("submit", function (event) {
     }
     },
     false,
-);
+)
+
+
